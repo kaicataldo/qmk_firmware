e22bf97be28aa299b78319936cbe8edaf82ff628
Kai Cataldo <kai@kaicataldo.com>
2021-11-08 11:59:14 -0500

    fix: KC_BSPC -> KC_BSPS

--- keyboards/jd45/keymaps/kaicataldo/keymap.c
+++ keyboards/jd45/keymaps/kaicataldo/keymap.c
@@ -5,7 +5,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T, KC_Y,   KC_U, KC_I,    KC_O,    KC_P,    KC_GRV,  KC_MUTE,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,   KC_G, KC_H,   KC_J, KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B, KC_N,   KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-        XXXXXXX, KC_LGUI, KC_LALT, MO(1),   KC_BSPS,      KC_SPC,       MO(1),   KC_RALT, KC_RGUI, XXXXXXX
+        XXXXXXX, KC_LGUI, KC_LALT, MO(1),   KC_BSPC,      KC_SPC,       MO(1),   KC_RALT, KC_RGUI, XXXXXXX
     ),
     [1] = LAYOUT(
         _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,  KC_9,    KC_0,    _______, _______,
@@ -14,10 +14,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         XXXXXXX, _______, _______, MO(2),   KC_ESC,           KC_ENT,           MO(2), _______, _______, XXXXXXX
     ),
     [2] = LAYOUT(
-        _______, LSFT(KC_1),   LSFT(KC_2),   LSFT(KC_3),    LSFT(KC_4),    LSFT(KC_5),    LSFT(KC_6), LSFT(KC_7), LSFT(KC_8), LSFT(KC_9), LSFT(KC_0), _______, _______,
+        _______, LSFT(KC_1),   LSFT(KC_2),   LSFT(KC_3),     LSFT(KC_4),    LSFT(KC_5),    LSFT(KC_6), LSFT(KC_7), LSFT(KC_8), LSFT(KC_9), LSFT(KC_0), _______, _______,
         _______, LSFT(KC_MINS), LSFT(KC_EQL), LSFT(KC_LBRC), LSFT(KC_RBRC), LSFT(KC_BSLS), KC_HOME,    KC_PGDN,    KC_PGUP,    KC_END,     _______,    _______,
-        _______, _______,      _______,      _______,       _______,       _______,       _______,    _______,    _______,    _______,    _______ ,   _______,
-        XXXXXXX, _______,      _______,      XXXXXXX,       KC_ESC,                       KC_BSPC,                XXXXXXX,    _______,    _______,    XXXXXXX
+        _______, _______,      _______,      _______,        _______,       _______,       _______,    _______,    _______,    _______,    _______ ,   _______,
+        XXXXXXX, _______,      _______,      XXXXXXX,        _______,                      _______,                XXXXXXX,    _______,    _______,    XXXXXXX
     ),
 };
 
@@ -25,16 +25,16 @@ bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 2) {
         if (layer_state_is(0)) {
             if (clockwise) {
-                tap_code(KC_VOLU);
+                tap_code(KC_WH_D);
             } else {
-                tap_code(KC_VOLD);
+                tap_code(KC_WH_U);
             }
         }
         if (layer_state_is(1)) {
             if (clockwise) {
-                tap_code(KC_WH_D);
+                tap_code(KC_VOLU);
             } else {
-                tap_code(KC_WH_U);
+                tap_code(KC_VOLD);
             }
         }
     }
